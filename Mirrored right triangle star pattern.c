3080525afd83c935556102db3ef3dec5b9d25a6e
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 16:36:49 -0600

    Create Mirrored right triangle star pattern.c

---
+++ Mirrored right triangle star pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i, j, r;
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+ 
+    for(i=1; i<=r; i++)
+    {
+        for(j=i; j<r; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+ 
+        printf("\n");
+    }    
+    return 0;
+} 
